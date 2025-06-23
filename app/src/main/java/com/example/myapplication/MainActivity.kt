8921c6abe3b3541c3a54e76decae1329ab1833ca
二刺螈 <i@songe.li>
2025-06-23 15:41:17 +0800

    feat: exceptionFlow

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,18 +8,23 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.hjq.permissions.OnPermissionCallback
 import com.hjq.permissions.Permission
 import com.hjq.permissions.XXPermissions
+import kotlinx.coroutines.flow.MutableStateFlow
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,12 +34,26 @@ class MainActivity : ComponentActivity() {
             MaterialTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Column(
-                        modifier = Modifier.fillMaxSize().padding(innerPadding),
-                        horizontalAlignment = Alignment.CenterHorizontally
+                        modifier = Modifier
+                            .verticalScroll(rememberScrollState())
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        horizontalAlignment = Alignment.CenterHorizontally,
                     ) {
                         Spacer(modifier = Modifier.height(40.dp))
                         Button(onClick = { requestMyPermissions() }) {
                             Text(text = "申请权限")
+                        }
+                        val exception = exceptionFlow.collectAsState().value
+                        if (exception != null) {
+                            Spacer(modifier = Modifier.height(20.dp))
+                            Text(
+                                text = exception.stackTraceToString(),
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(horizontal = 20.dp),
+                                color = MaterialTheme.colorScheme.error,
+                            )
                         }
                     }
                 }
@@ -42,24 +61,36 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    val exceptionFlow = MutableStateFlow<Throwable?>(null)
+
     val context = this
 
     fun requestMyPermissions() {
-        XXPermissions.with(context)
-            .unchecked()
-            .permission(Permission.POST_NOTIFICATIONS)
-            .request(object : OnPermissionCallback {
-                override fun onGranted(permissions: MutableList<String>, allGranted: Boolean) {
-                    Toast.makeText(context, "权限申请成功", Toast.LENGTH_SHORT).show()
-                }
-
-                override fun onDenied(permissions: MutableList<String>, doNotAskAgain: Boolean) {
-                    if (doNotAskAgain) {
-                        Toast.makeText(context, "权限被拒绝且不再询问", Toast.LENGTH_SHORT).show()
-                    } else {
-                        Toast.makeText(context, "权限申请失败", Toast.LENGTH_SHORT).show()
+        try {
+            XXPermissions.with(context)
+                .unchecked()
+                .permission(Permission.POST_NOTIFICATIONS)
+                .request(object : OnPermissionCallback {
+                    override fun onGranted(permissions: MutableList<String>, allGranted: Boolean) {
+                        Toast.makeText(context, "权限申请成功", Toast.LENGTH_SHORT).show()
                     }
-                }
-            })
+
+                    override fun onDenied(
+                        permissions: MutableList<String>,
+                        doNotAskAgain: Boolean
+                    ) {
+                        if (doNotAskAgain) {
+                            Toast.makeText(context, "权限被拒绝且不再询问", Toast.LENGTH_SHORT)
+                                .show()
+                        } else {
+                            Toast.makeText(context, "权限申请失败", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                })
+            exceptionFlow.value = null
+        } catch (e: Throwable) {
+            exceptionFlow.value = e
+            Toast.makeText(context, "发生错误: ${e.message}", Toast.LENGTH_SHORT).show()
+        }
     }
 }
